4a8247d6120adfa20488d7e81d763746802ca1a5
lkzkr0w <biusolucas@protonmail.com>
2022-05-04 00:38:50 -0300

    expanded some functions

--- pygen.py
+++ pygen.py
@@ -4,9 +4,14 @@ import itertools
 import sys
     
 def make_wordlist(base_list):
-    #TODO
-    
+    long_word = ''
+    while len(base_list) > 0:
+        long_word += base_list.pop()
+    print(long_word)                        #debug print
 
+    # test_wordlist = list(itertools.permutations(long_word))
+    # print(test_wordlist)                    #debug print
+    
 def main():
     keywords = sys.argv[1]
     keywords_list = keywords.split(',')
